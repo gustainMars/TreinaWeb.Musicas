561bb2c5029d6cae66f224930c789295361a258d
gustasDesktop <gustaDesktop@github.com>
2021-05-09 19:31:08 -0300

    Criado Dominio e a classe POCO Album.

---
+++ TreinaWeb.Musicas.Dominio/Album.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TreinaWeb.Musicas.Dominio
+{
+    public class Album
+    {//Classe POCO, simples, sem dependências externas.
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataLancamento { get; set; }
+        public string Observacoes { get; set; }
+    }
+}
