bb2f03f2dc7cb184920da3a80411912de000ea0c
gustasDesktop <gustaDesktop@github.com>
2021-05-11 13:53:54 -0300

    Criado classe abstrata para especificar uma configuração para geração das tabelas por parte do Entity Framework.

---
+++ TreinaWeb.Comum.Entity/TreinaWebEntityAbstractConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinaWeb.Comum.Entity
+{
+    public abstract class TreinaWebEntityAbstractConfig<TEntidade> : EntityTypeConfiguration<TEntidade>
+        where TEntidade : class
+    {
+        public TreinaWebEntityAbstractConfig()
+        {
+            ConfigurarNomeTabela();
+            ConfigurarCamposTabela();
+            ConfigurarChavePrimaria();
+            ConfigurarChaveEstrangeira();
+        }
+
+        protected abstract void ConfigurarChaveEstrangeira();
+        protected abstract void ConfigurarChavePrimaria();
+        protected abstract void ConfigurarCamposTabela();
+        protected abstract void ConfigurarNomeTabela();
+    }
+}
