34a92194def75c20901a7f4f7ff7605fa479de87
gustasDesktop <gustaDesktop@github.com>
2021-05-11 13:55:52 -0300

    Implementação da classe TreinaWebEntityAbstractConfig para a tabela Album.

---
+++ TreinaWeb.Musicas.AcessoDados.Entity/TypeConfiguration/AlbumTypeConfiguration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreinaWeb.Comum.Entity;
+using TreinaWeb.Musicas.Dominio;
+
+namespace TreinaWeb.Musicas.AcessoDados.Entity.TypeConfiguration
+{
+    class AlbumTypeConfiguration : TreinaWebEntityAbstractConfig<Album>
+    {
+        protected override void ConfigurarCamposTabela()
+        {
+            Property(p => p.Id)
+                .IsRequired()
+                .HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)
+                .HasColumnName("alb_id");
+
+            Property(p => p.Nome)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasColumnName("alb_nome");
+
+            Property(p => p.DataLancamento)
+                .IsRequired()
+                .HasColumnName("alb_ano");
+
+            Property(p => p.Observacoes)
+                .IsOptional()
+                .HasColumnName("alb_observacoes")
+                .HasMaxLength(800);
+        }
+
+        protected override void ConfigurarChaveEstrangeira()
+        {
+           
+        }
+
+        protected override void ConfigurarChavePrimaria()
+        {
+            HasKey(pk => pk.Id);
+        }
+
+        protected override void ConfigurarNomeTabela()
+        {
+            ToTable("alb_albuns");
+        }
+    }
+}
